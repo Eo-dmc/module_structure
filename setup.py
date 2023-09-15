fac603a5cecd204b64875faeaca7ad5d93f0d2dd
eo.dmc <emigdiodmc@gmail.com>
2023-09-15 12:00:16 +0200

    Included basic settings

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from module_structure import __author__,__version__,__name__
+
+VERSION = __version__
+AUTHOR = __author__
+NAME = __name__
+
+setup(
+    name                        = NAME,
+    version                     = VERSION,
+    description                 = "Brief description of your package",
+    author                      = AUTHOR,
+    author_email                = 'emigdiodmc@gmail.com',
+    license                     = 'MIT',
+    python_requires             = '>=3.9.5',
+    packages                    = find_packages(),
+    include_package_data        = True,
+    package_data                = {'': ['resources/*.csv','resources/clusters/*.csv']},
+    install_requires            = [
+                                    'pandas',
+                                    'numpy',
+                                    'tensorflow-gpu',
+                                    'torch'
+                                    ]
+    )
